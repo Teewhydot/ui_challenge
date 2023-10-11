bd27c7b68c2c06c2356d25e585aa646f323e348b
Tunde <tchipsical@gmail.com>
2023-10-11 21:21:37 +0100

    Done working on search result items page and details page.

--- lib/search_results_items.dart
+++ lib/search_results_items.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_challenge/generated/assets.dart';
 
 class ContainerGrid extends StatelessWidget {
   const ContainerGrid({super.key});
@@ -16,28 +17,63 @@ class ContainerGrid extends StatelessWidget {
           clipBehavior: Clip.none,
           children: [
             Positioned(
-              top: 125,
-              child: Container(// Change the color as desired.
-                height: 250.0, // Adjust the height and width as needed.
-                width: 150.0,
-                margin: const EdgeInsets.only(left: 40),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.blue, // Change the color as desired.
-                ),
-                // Adjust the margin as needed.
-              ),
+              top: 75,
+              child: Container(
+                  // Change the color as desired.
+                  height: 250.0, // Adjust the height and width as needed.
+                  width: 150.0,
+                  margin: const EdgeInsets.only(left: 40),
+                  decoration: BoxDecoration(
+                    color: Colors.white30, // Change the color as desired.
+
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Image.asset(Assets.imagesPlant2),
+                        const Text('Product Name'),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Product Price'),
+                            Icon(Icons.favorite_border),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )),
+              // Adjust the margin as needed.
             ),
             Positioned(
               right: 0,
               child: Container(
                 height: 250.0, // Adjust the height and width as needed.
-                width:150.0,
+                width: 150.0,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
-                  color: Colors.blue, // Change the color as desired.
+                  color: Colors.white30, // Change the color as desired.
                 ),
-                margin: const EdgeInsets.only(right: 40), // Adjust the margin as needed.
+                margin: const EdgeInsets.only(right: 40),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(Assets.imagesPlant4),
+                      const Text('Product Name'),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Product Price'),
+                          Icon(Icons.favorite_border),
+                        ],
+                      ),
+                    ],
+                  ),
+                ), // Adjust the margin as needed.
               ),
             ),
           ],
